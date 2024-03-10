95defcbbadcf4b8d7fc8f0d86cf15507aee952be
fasterplaymc <agolobokov1@gmail.com>
2024-03-10 21:17:40 +0700

    From Figma

---
+++ app/src/test/java/com/agolobokovsapplication/app/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.agolobokovsapplication.app
+
+import com.agolobokovsapplication.app.appcomponents.base.BaseActivity
+import com.agolobokovsapplication.app.databinding.LayoutProgressDialogBinding
+
+class MainActivity : BaseActivity<LayoutProgressDialogBinding>(R.layout.layout_progress_dialog) {
+
+    override fun onInitialized() {
+
+    }
+
+    override fun setUpClicks() {
+
+    }
+}
